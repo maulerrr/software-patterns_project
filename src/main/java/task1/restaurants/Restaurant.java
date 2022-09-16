4b4a56153b278a2848cca0ec5a9adcc32b743f0d
Moonapso <211257@astanait.edu.kz>
2022-09-16 13:54:09 +0600

    added payment

--- src/main/java/task1/restaurants/Restaurant.java
+++ src/main/java/task1/restaurants/Restaurant.java
@@ -1,6 +1,11 @@
 package task1.restaurants;
 
+import task1.payment.PaymentStrategy;
+
 public abstract class Restaurant {
+    private PaymentStrategy paymentStrategy;
     void cookFood(){}
-    void pay(){}
+    void pay(int price){
+        paymentStrategy.pay(price);
+    }
 }
