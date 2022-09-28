a46c22e19474f6f720383e860b2f8911acc49eb1
maulerrr <211239@astanait.edu.kz>
2022-09-29 01:29:56 +0600

    Notification methods for Delivery

--- src/main/java/task1/delivery/DeliveryStrategy.java
+++ src/main/java/task1/delivery/DeliveryStrategy.java
@@ -1,5 +1,23 @@
 package task1.delivery;
 
-public interface DeliveryStrategy {
+import task1.observables.SubscribersNotificationSender;
+import task1.observer.Observer;
+
+public interface DeliveryStrategy extends SubscribersNotificationSender {
     void deliver();
+
+    @Override
+    default void subscribe(Observer observer) {
+
+    }
+
+    @Override
+    default void unsubscribe(Observer observer) {
+
+    }
+
+    @Override
+    default void sendSubscribersNotification(String message, Observer observer) {
+
+    }
 }
--- src/main/java/task1/delivery/YandexDelivery.java
+++ src/main/java/task1/delivery/YandexDelivery.java
@@ -1,8 +1,25 @@
 package task1.delivery;
 
+import task1.observer.Observer;
+
 public class YandexDelivery implements DeliveryStrategy{
     @Override
     public void deliver() {
         System.out.println("Delivery by Yandex");
+    }
+
+    @Override
+    public void subscribe(Observer observer) {
+        DeliveryStrategy.super.subscribe(observer);
+    }
+
+    @Override
+    public void unsubscribe(Observer observer) {
+        DeliveryStrategy.super.unsubscribe(observer);
+    }
+
+    @Override
+    public void sendSubscribersNotification(String message, Observer observer) {
+        DeliveryStrategy.super.sendSubscribersNotification(message, observer);
     }
 }
